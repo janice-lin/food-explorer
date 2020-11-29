bd29d9304aa790003fd8af8249097d451e024665
janice-lin <janicemail0401@gmail.com>
2020-11-29 01:11:20 -0800

    partials and fixed some html

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,1 +1,8 @@
 // Write your JavaScript here...
+
+const body = document.body;
+const btn = document.querySelector('.nav-btn');
+
+btn.addEventListener('click', function(){
+    body.classList.toggle('show');
+});
